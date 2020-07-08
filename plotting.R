12faa3c4c2099ef48431830a96ac4aeb456f747b
DBomber60 <davidbreynolds@gmail.com>
2020-07-08 07:42:45 -0400

    p

---
+++ plotting.R
@@ -0,0 +1,22 @@
+# spike and slab regularization plot
+nu_0_seq = seq(from = 0.1, to = 0.5, by = .01)
+res = matrix(nrow = p+1, ncol = length(nu_0_seq))
+colnames(res) = paste("nu0",nu_0_seq)
+a = data.frame(res)
+a$var = paste("beta",1:(p+1))
+
+# now, fill in the data
+
+for (m in 1:length(nu_0_seq)) {
+  em.iter = EMVS(beta_0, theta_0, nu_0_seq[m], nu_1)
+  print(em.iter[[3]] )
+  a[,m] = em.iter[[1]]
+}
+
+long = pivot_longer(a, cols = starts_with("nu0"), values_to = "beta")
+long$nu = rep(nu_0_seq, p+1)
+
+ggplot(data = long, aes(x=nu, y=beta[,1])) +
+  geom_line(aes(colour=var)) +
+  theme_bw()
+
