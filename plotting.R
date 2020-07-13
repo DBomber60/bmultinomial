eae56ce976d82308c141aa72bf9ac79dadc7cfa3
DBomber60 <davidbreynolds@gmail.com>
2020-07-13 11:24:39 -0400

    u

--- plotting.R
+++ plotting.R
@@ -1,10 +1,10 @@
 # spike and slab regularization plot
 theta_0 = 0.5
-nu_0_seq = seq(from = 0.1, to = 2, by = .1)
+nu_0_seq = seq(from = 0.01, to = 1, by = .01)
 res = matrix(nrow = p+1, ncol = length(nu_0_seq))
 colnames(res) = paste("nu0",nu_0_seq)
 a = data.frame(res)
-a$var = factor(1:11) #paste("beta",1:(p+1))
+a$var = factor(1:(p+1)) #paste("beta",1:(p+1))
 
 # now, fill in the data
 
@@ -19,6 +19,6 @@ long$nu = rep(nu_0_seq, p+1)
 
 ggplot(data = long, aes(x=nu, y=beta[,1])) +
   geom_line(aes(colour=var)) +
-  scale_color_manual(values=c( rep('#E69F00', 6), rep('#999999', 5)) ) +
+  scale_color_manual(values=c( rep('#E69F00', 6), rep('#999999', (p-5))) ) +
   theme_bw()
 
