19f032844e5e5d9fb440a17c89d49b7f1e4cda34
DBomber60 <davidbreynolds@gmail.com>
2020-07-13 11:13:57 -0400

    em

--- emvs.R
+++ emvs.R
@@ -8,20 +8,14 @@ nr.beta = function(beta.init, phat.init, dstar) {
   while(dnr.ll > 1e-6) {
     beta.old = beta.current
     U = t(X) %*% (Y - phat.current) - dstar * beta.current
-    H = solve(diag(dstar) + t(as.numeric(phat.current * (1-phat.current)) * X ) %*% X)
+    H = solve( diag(as.numeric(dstar)) + t(as.numeric(phat.current * (1-phat.current)) * X ) %*% X)
     beta.current = beta.current + H %*% U
     eta.current = X %*% beta.current
     phat.current = plogis(eta.current)
-    
     dnr.ll = sqrt(crossprod(beta.current - beta.old))
-    
   }
-  
   return(beta.current)
-  
 }
-
-
 
 
 # implementation of the EMVS procedure of Rockava and George, 2014
@@ -33,37 +27,19 @@ EM.iter = function(beta.current, theta.current, nu_0, nu_1) {
   # when gamma_i=1, beta_i ~ N(0, v_1); beta_i is a scale mixture of normals (spike + slab)
   p1 = dnorm(beta.current, mean = 0, sd = sqrt(nu_1)) * theta.current
   p0 = dnorm(beta.current, mean = 0, sd = sqrt(nu_0)) * (1-theta.current)
-  #print(p0)
   
   pstar = p1/(p0 + p1)
-  #print(pstar)
-  
   dstar = pstar/nu_1 + (1-pstar)/nu_0
-  print(dim(diag(dstar)))
   
   # M-Step
   
-  # one step of Newtwon Rhapson to update beta vector
-  #eta.current = X %*% beta.current
-  #phat.current = plogis(eta.current)
+  # maximize beta using Newtwon Rhapson
   
   beta.new = nr.beta(rep(0, p+1), phat.init = rep(.5, n), dstar)
-  # for (nr in 1:20) {
-  #   U = t(X) %*% (Y - phat.current) - dstar * beta.current
-  #   H = solve(diag(dstar) + t(as.numeric(phat.current * (1-phat.current)) * X ) %*% X)
-  #   beta.current = beta.current + H %*% U
-  #   eta.current = X %*% beta.current
-  #   phat.current = plogis(eta.current)
-  # }
-  
-  #beta.new = beta.current
-  
+
   # now, update theta
   theta.new = sum(pstar)/(p+1) # assumes a=b=1
-  #print(theta.new)
-  
   return(list(beta=beta.new, theta = theta.new, pstar = pstar))
-  
 }
 
 
@@ -73,7 +49,6 @@ EMVS <- function(beta.init, theta.init, nu_0, nu_1) {
   theta.current = theta.init
   
   delta.ll <- 1
-  #  for (i in 1:3)
   while(delta.ll > 1e-6)  {
     beta.old = beta.current
     it = EM.iter(beta.current, theta.current, nu_0, nu_1)
@@ -82,6 +57,5 @@ EMVS <- function(beta.init, theta.init, nu_0, nu_1) {
     delta.ll = sqrt(crossprod(beta.current - beta.old))
     pstar = it$pstar
   }
-  
   return(list(beta.current, theta.current, pstar))
 }
